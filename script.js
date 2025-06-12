b091e6088d31dedd351f0dc737cf3c2d1cdbd570
Poojarimohan <poojarimohan294@gmail.com>
2025-06-13 00:38:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,21 @@ const toCurrencySelect = document.getElementById("to-currency");
 const convertButton = document.getElementById("convert-btn");
 const resultDiv = document.getElementById("result");
 
-// Sample API Key (replace this later with your own)
 const apiKey = "71fe3292ba781811dd3c9fb0";
 
-// Add event listener to the Convert button
 convertButton.addEventListener("click", () => {
     const amount = amountInput.value;
     const fromCurrency = fromCurrencySelect.value;
     const toCurrency = toCurrencySelect.value;
 
-    // Basic input check
     if (amount === "" || isNaN(amount)) {
         resultDiv.innerText = "⚠️ Please enter a valid amount.";
         return;
     }
 
-    // Construct API URL
+    
     const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`;
 
-    // Fetch exchange rate
     fetch(url)
         .then(response => response.json())
         .then(data => {
